74f8bfe682490483798a6c4477c3116a42976b06
Disha <suledisha1@ccs.neu.edu>
2018-06-28 03:29:22 -0400

    check added to authored-books

--- src/app/author-book-list/author-book-list.component.ts
+++ src/app/author-book-list/author-book-list.component.ts
@@ -12,24 +12,47 @@ export class AuthorBookListComponent implements OnInit {
 
   volumeId = '';
   authoredBooks = [];
+  alreadyauthored = 0;
   constructor(private service: AuthoredBookListServiceClient,
               private bookDetailService: BookDetailsServiceClient,
               private bookService: BookServiceClient) { }
 
   addToAuthoredBooks() {
     this.bookDetailService.findBookById(this.volumeId)
-      .then(book => {
-        this.bookService.createBook(book.id, book.volumeInfo.title)
-          .then(newbook => {
-          this.service.addToAuthoredBooks(newbook._id);
-        })
-          .then(authored => {
-          this.service
-            .findAuthoredBooksForUser()
-            .then(authoredBooks => this.authoredBooks = authoredBooks );
-        });
+      .then(foundbook => {
+        this.bookService.findBookByCredential(this.volumeId)
+          .then(book => {
+            if (book._id === '-1') {
+              this.bookService.createBook(foundbook.id, foundbook.volumeInfo.title)
+                .then(newbook => {
+                  this.service.addToAuthoredBooks(newbook._id);
+                })
+                .then(authored => {
+                  this.service
+                    .findAuthoredBooksForUser()
+                    .then(authoredBooks => this.authoredBooks = authoredBooks);
+                });
+            } else {
+              for (const entry of this.authoredBooks) {
+                if (entry.book._id === book._id) {
+                  this.alreadyauthored = 1;
+                }
+              }
+              if (this.alreadyauthored === 0) {
+                this.service.addToAuthoredBooks(book._id)
+                  .then(authored => {
+                    this.service
+                      .findAuthoredBooksForUser()
+                      .then(authoredBooks => this.authoredBooks = authoredBooks);
+                  });
+              } else {
+                alert('Already authored!');
+              }
+            }
+          });
       });
   }
+
   removeFromAuthoredBooks(bookId) {
     this.service.removeFromAuthoredBooks(bookId)
       .then(authored => {
